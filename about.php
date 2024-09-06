c420f4e5c56cea855d5ca28b6a4acd20e42b20db
Resti Aulia Sari <restiauliasari2702@gmail.com>
2024-09-06 19:39:49 +0700

    mengubah halaman home

--- about.php
+++ about.php
@@ -18,7 +18,7 @@
     </header>
 
     <section>
-        <h2>Tentang Project</h2>
+        <h2>Selamat Datang Di SMK</h2>
         <p>Project ini dibuat sebagai contoh project PHP Native sederhana tanpa menggunakan database.</p>
         <p>Tujuan project ini adalah untuk menunjukkan cara kerja dasar PHP dalam menampilkan data yang didefinisikan secara manual.</p>
     </section>
